5ccff3c4ca337e09b8b1d83f00a48014d07d8042
Jean <dot.com>
2026-02-24 07:41:29 +0100

    arkenfox

--- flake.nix
+++ flake.nix
@@ -3,21 +3,31 @@
 
   inputs =  {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    home-manager.url = "github:nix-community/home-manager/release-25.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+    	url = "github:nix-community/home-manager/release-25.11";
+    	inputs.nixpkgs.follows = "nixpkgs";
+    };
+    arkenfox-nixos.url = "github:dwarfmaster/arkenfox-nixos";
   };
 
-  outputs = { self, nixpkgs, home-manager, ...}@inputs: {
+  outputs = { self, nixpkgs, home-manager, arkenfox-nixos, ...}: {
     nixosConfigurations.nix-dell = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = { inherit inputs; };
+      # specialArgs = { inherit inputs; };
       modules = [
         ./configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.Jean = import ./home.nix;
+          home-manager.users.Jean = {
+	          imports = [
+	          	./home.nix
+	          	# ./modules/desktop/firefox.nix
+	          	arkenfox-nixos.hmModules.default
+	          ];
+	      };
+          # _module.args = { inherit arkenfox-nixos; };
         }
       ];
     };
--- modules/desktop/firefox.nix
+++ modules/desktop/firefox.nix
@@ -1,12 +1,36 @@
-{pkgs, ...}:
+{ pkgs, ...}:
 
 {
+	# imports = [
+	# 	arkenfox-nixos.hmModules.default
+	# ];
+
 	programs.firefox = {
 		enable = true;
+
+		arkenfox = {
+			enable = true;
+			version = "master";
+		};
+		
 		profiles.Hard = {
+			id = 0;
+			name = "Hard";
 			isDefault = true;
+
+			arkenfox = {
+				enable = true;
+				"0100".enable = true; # Startup settings
+				"0200".enable = true; # Geolocation
+				"0400".enable = true; # Safe Browsing
+				"0800".enable = true; # Search Bar
+				"1000".enable = true; # Disk avoidance ( attention, réduit le cache)
+				"2600".enable = true; # Netttoyage du shutdown
+				"2700".enable = true; # Protection contre le pistage
+			};
+			
 			settings = {
-				"browser.startup.homepage" = "https://nixos.org";
+				# "browser.startup.homepage" = "https://nixos.org";
 				"browser.search.region" = "FR";
 				"browser.search.isUS" = false;
 				"distribution.searchplugins.defaultLocale" = "fr_FR";
@@ -17,6 +41,13 @@
 				"privacy.fingerprintingProtection" = true;
 				"dom.battery.enabled" = "false";
 				"browser.newtabpage.enabled" = false;
+
+				"browser.uidensity" = 1; # Mode compact
+
+				# Optimisation pour GPU ancien :
+				"gfx.webrender.all" = true;
+				"layers.acceleration.force-enabled" = true;
+				# "privacy.resistFingerprinting" = false; # Concomme trop de CPU
 			};
 		};
 	};
